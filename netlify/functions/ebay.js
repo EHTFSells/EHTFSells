7d9d2bab0f534351827a59145435494c6144b00e
EHTFSells <elliothaydon01@gmail.com>
2026-06-08 17:38:57 +0100

    Update ebay.js

--- netlify/functions/ebay.js
+++ netlify/functions/ebay.js
@@ -1,5 +1,4 @@
 const https = require('https');
-const url = require('url');
 
 exports.handler = async function(event) {
   if (event.httpMethod === 'OPTIONS') {
@@ -15,32 +14,65 @@ exports.handler = async function(event) {
   }
 
   try {
-    const { callName, body } = JSON.parse(event.body);
     const APP_ID = 'ElliotHa-EHTFSell-PRD-d9968b8a6-1379ff01';
-
-    const xmlResponse = await new Promise((resolve, reject) => {
+    
+    // First get an OAuth app token
+    const tokenResponse = await new Promise((resolve, reject) => {
+      const credentials = Buffer.from(`${APP_ID}:ElliotHa-EHTFSell-PRD-d9968b8a6-1379ff01`).toString('base64');
+      const postData = 'grant_type=client_credentials&scope=https%3A%2F%2Fapi.ebay.com%2Foauth%2Fapi_scope';
+      
       const options = {
         hostname: 'api.ebay.com',
-        path: '/ws/api.dll',
+        path: '/identity/v1/oauth2/token',
         method: 'POST',
         headers: {
-          'Content-Type': 'text/xml',
-          'Content-Length': Buffer.byteLength(body),
-          'X-EBAY-API-SITEID': '3',
-          'X-EBAY-API-COMPATIBILITY-LEVEL': '967',
-          'X-EBAY-API-CALL-NAME': callName,
-          'X-EBAY-API-APP-NAME': APP_ID,
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': `Basic ${credentials}`,
+          'Content-Length': Buffer.byteLength(postData)
         }
       };
 
       const req = https.request(options, (res) => {
         let data = '';
         res.on('data', chunk => { data += chunk; });
-        res.on('end', () => resolve(data));
+        res.on('end', () => resolve(JSON.parse(data)));
       });
-
       req.on('error', reject);
-      req.write(body);
+      req.write(postData);
+      req.end();
+    });
+
+    console.log('Token response:', JSON.stringify(tokenResponse).substring(0, 200));
+
+    if (!tokenResponse.access_token) {
+      throw new Error('No access token: ' + JSON.stringify(tokenResponse));
+    }
+
+    // Search for seller's listings using Browse API
+    const searchResponse = await new Promise((resolve, reject) => {
+      const path = '/buy/browse/v1/item_summary/search?seller_username=elliohaydo_0&limit=50';
+      
+      const options = {
+        hostname: 'api.ebay.com',
+        path: path,
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${tokenResponse.access_token}`,
+          'X-EBAY-C-MARKETPLACE-ID': 'EBAY_GB',
+          'Content-Type': 'application/json'
+        }
+      };
+
+      const req = https.request(options, (res) => {
+        let data = '';
+        console.log('Browse API status:', res.statusCode);
+        res.on('data', chunk => { data += chunk; });
+        res.on('end', () => {
+          console.log('Browse API response:', data.substring(0, 500));
+          resolve({ status: res.statusCode, data: data });
+        });
+      });
+      req.on('error', reject);
       req.end();
     });
 
@@ -48,12 +80,13 @@ exports.handler = async function(event) {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'text/xml'
+        'Content-Type': 'application/json'
       },
-      body: xmlResponse
+      body: searchResponse.data
     };
 
   } catch (err) {
+    console.log('Error:', err.message);
     return {
       statusCode: 500,
       headers: { 'Access-Control-Allow-Origin': '*' },
