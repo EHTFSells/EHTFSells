32181e6dfe7505755b3abf073eb038a333b6c5c2
EHTFSells <elliothaydon01@gmail.com>
2026-06-08 17:31:48 +0100

    Update ebay.js

--- netlify/functions/ebay.js
+++ netlify/functions/ebay.js
@@ -1,3 +1,6 @@
+const https = require('https');
+const url = require('url');
+
 exports.handler = async function(event) {
   if (event.httpMethod === 'OPTIONS') {
     return {
@@ -15,19 +18,31 @@ exports.handler = async function(event) {
     const { callName, body } = JSON.parse(event.body);
     const APP_ID = 'ElliotHa-EHTFSell-PRD-d9968b8a6-1379ff01';
 
-    const response = await fetch('https://api.ebay.com/ws/api.dll', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'text/xml',
-        'X-EBAY-API-SITEID': '3',
-        'X-EBAY-API-COMPATIBILITY-LEVEL': '967',
-        'X-EBAY-API-CALL-NAME': callName,
-        'X-EBAY-API-APP-NAME': APP_ID,
-      },
-      body: body
-    });
+    const xmlResponse = await new Promise((resolve, reject) => {
+      const options = {
+        hostname: 'api.ebay.com',
+        path: '/ws/api.dll',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'text/xml',
+          'Content-Length': Buffer.byteLength(body),
+          'X-EBAY-API-SITEID': '3',
+          'X-EBAY-API-COMPATIBILITY-LEVEL': '967',
+          'X-EBAY-API-CALL-NAME': callName,
+          'X-EBAY-API-APP-NAME': APP_ID,
+        }
+      };
 
-    const text = await response.text();
+      const req = https.request(options, (res) => {
+        let data = '';
+        res.on('data', chunk => { data += chunk; });
+        res.on('end', () => resolve(data));
+      });
+
+      req.on('error', reject);
+      req.write(body);
+      req.end();
+    });
 
     return {
       statusCode: 200,
@@ -35,8 +50,9 @@ exports.handler = async function(event) {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'text/xml'
       },
-      body: text
+      body: xmlResponse
     };
+
   } catch (err) {
     return {
       statusCode: 500,
